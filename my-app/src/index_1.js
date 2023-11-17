e52294d2c039a5608198389d38605310121b381b
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2023-11-17 09:31:46 +0700

    Create index_1.js

---
+++ my-app/src/index_1.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+function Welcome(){
+    return (
+        <section>
+            <h1>Hello world😍😘</h1>
+        </section>
+    );
+}
+function App(){
+    return (
+        <div className='App'>
+            <Welcome/>
+        </div>
+        );
+}
+function App(){
+    return (
+        <div className='App'>
+            <Welcome/>
+            <Welcome/>
+            <Welcome/>
+        </div>
+        );
+}
+export default function MyApp(){
+    return (
+        <div className='App'>
+            <Welcome/>
+            <Welcome/>
+            <Welcome/>
+        </div>
+    )
+}
